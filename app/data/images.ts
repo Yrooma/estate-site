864f620cc6de2cca63b66fbf86a3e3d64fe829d1
Yrooma <o_mxd@hotmail.com>
2024-12-31 23:54:19 +0300

    الموقع والصور

--- app/data/images.ts
+++ app/data/images.ts
@@ -1,11 +1,11 @@
 export const images = {
   services: {
     documentation: '/images/services/s33.jpg',
-    realEstateBrokerage: '/images/services/s4.jpg',
-    propertyValuation: '/images/services/s2.jpg',
-    propertyManagement: '/images/services/s1.jpg',
-    generalServices: '/images/services/s5.jpg',
-    realEstateConsultancy: '/images/services/s2.jpg',
+    realEstateBrokerage: '/images/services/s44.jpg',
+    propertyValuation: '/images/services/s22.jpg',
+    propertyManagement: '/images/services/s1.svg',
+    generalServices: '/images/services/s55.jpg',
+    realEstateConsultancy: '/images/services/s22.jpg',
   },
   properties: {
     property1: '/images/properties/property1.jpg',
